e6536316bfb2e82fb1fa5a06bf25864700f3ecb0
gjgd <gjgd@protonmail.com>
2021-03-08 15:33:05 +0000

    chore: fix indent

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const venom = require('venom-bot');
 
 venom
-	.create(
-		'session',
-		() => {},
-		() => {},
-		{
-			browserArgs: ['--no-sandbox'],
-		}
-	).then((client) => start(client))
-	.catch((error) => {
-		console.error(error);
-	});
+  .create(
+    'session',
+    () => {},
+    () => {},
+    {
+      browserArgs: ['--no-sandbox'],
+    }
+  ).then((client) => start(client))
+  .catch((error) => {
+    console.error(error);
+  });
 
 function start(client) {
   client.onMessage(async (message) => {
